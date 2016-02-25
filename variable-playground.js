4c71eb96a2c27e19056f73964b440aa34129d642
Luba May <Liubov.Mayorova@gmail.com>
2016-02-24 19:59:07 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+//var veggie = {
+//  name: "Spinach",
+//    age: 3
+//};
+//
+//function updateVeggie (obj) {
+//    //obj = {
+//    //    name: "Spinach",
+//    //    age: 1
+//    //}
+//
+//    obj.age = 1;
+//}
+//
+//updateVeggie(veggie);
+//console.log(veggie);
+
+// Array Example
+
+var grades = [15, 37];
+
+function addGrades (gradesArr) {
+   gradesArr.push(40);
+    debugger;
+    //gradesArr = [15, 37, 40];
+}
+
+
+addGrades(grades);
+console.log(grades);
