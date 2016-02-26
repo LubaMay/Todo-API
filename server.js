9901af4d41ea2cb43a5ab34246fe5a33b09c669d
Luba May <Liubov.Mayorova@gmail.com>
2016-02-26 16:36:58 -0500

    Convert Get /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -43,13 +43,17 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    if(matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send('This id doesn\'t exist');
-    }
+    db.todo.findById(todoId).then(function (todo) {
+        if(todo){
+             res.json(todo.toJSON());
+        } else {
+            res.status(404).send();
+        }
+    }).catch(function (error){
+        res.status(500).send(error);
+    });
+
 
 });
 
@@ -65,17 +69,6 @@ app.post('/todos', function (req, res) {
 
     });
 
-    //if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //    return res.status(400).send();
-    //}
-    //
-    //body.description = body.description.trim();
-    //
-    //body.id = todoNextId++;
-    //
-    //todos.push(body);
-    //
-    //res.json(body);
 });
 
 // DELETE /todos/:id
