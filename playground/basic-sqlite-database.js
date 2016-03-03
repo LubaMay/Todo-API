92cced0530fb65bc625ef344ff00431adac6ee3c
Luba May <Liubov.Mayorova@gmail.com>
2016-03-03 16:25:47 -0500

    Add todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,22 +19,55 @@ var Todo = sequelize.define('todo', {
         }
 });
 
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
 
 sequelize.sync({
     //force: true
     }).then(function () {
     console.log('Everything is synced');
 
-    Todo.findById(2).then(function (todo) {
-        if (todo) {
-            console.log(todo.toJSON());
-        } else {
-            console.log('todo not found');
-        }
-    }).catch(function (error) {
-        console.log(error);
-    })
+    User.findById(1).then(function(user){
+       user.getTodos({
+           where: {
+               completed: false
+           }
+       }).then(function(todos){
+           todos.forEach(function(todo){
+              console.log(todo.toJSON());
+           });
+       });
+    });
+
+    //User.create({
+    //    email: 'brussel@sprouts.com'
+    //}).then(function (){
+    //    return Todo.create({
+    //        description: 'Eat brussel sprouts'
+    //    });
+    //}).then(function (todo){
+    //    User.findById(1).then(function (user){
+    //        user.addTodo(todo);
+    //    });
+    //});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     //Todo.create({
